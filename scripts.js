f4dd72719ddf2e9c4465c3f5ded625d78d15d6db
Penghao-99 <tph19999@gmail.com>
2023-04-07 16:45:29 +0800

    chatgpt-api
    Instructions: npm run dev, and prompt will appear, enter and then chatgpt will reply, then ask for another prompt

---
+++ scripts.js
@@ -0,0 +1,23 @@
+import {config} from "dotenv"
+config()
+
+import { Configuration, OpenAIApi } from "openai"
+import readline from "readline"
+
+const configuration = new Configuration({
+    apiKey: process.env.API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+const userInterface = readline.createInterface({input: process.stdin, output: process.stdout})
+
+userInterface.prompt()
+userInterface.on("line", async (input) => {
+    const res = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo",
+    messages: [{role: "user", content: input}],
+})
+
+console.log(res.data.choices[0].message.content)
+userInterface.prompt() // loop
+})
